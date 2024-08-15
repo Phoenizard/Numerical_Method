46e7fd574af458fdbf725a9bc4613dd3d2cc8ed2
Phoenizard <jiangxiaoyi2004@gmail.com>
2024-08-15 21:34:14 +0800

    fix bug in example 3

--- data/dataset_example3.py
+++ data/dataset_example3.py
@@ -1,44 +1,35 @@
 import numpy as np
 import torch
 
-# 设置维度和样本数
-dimension = 20
-num_samples = 100000
+D = 20
+M = 100000
 
-# 生成数据集
-data = np.random.normal(0, 0.2, (num_samples, dimension))
+# X取值为N(0, 0.2)
+X = np.random.randn(M, D) * 0.2
+X_z = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+X_bias = np.hstack([X_z, np.ones((M, 1))])
 
-# 添加偏置项（第41维）
-bias = np.ones((num_samples, 1))
-data_with_bias = np.hstack((data, bias))
+def target_function(x):
+    # exp(-10 * ||x||)
+    return np.exp(-10 * np.linalg.norm(x))
 
-# 数据划分为训练集和测试集
-indices = np.arange(num_samples)
-np.random.shuffle(indices)
-train_size = int(0.8 * num_samples)
-X_train = data_with_bias[indices[:train_size]]
-X_test = data_with_bias[indices[train_size:]]
+Y = np.array([])
+for x in X:
+    Y = np.append(Y, target_function(x))
 
-# 生成随机的目标值
-Y_train = np.random.randn(train_size)
-Y_test = np.random.randn(num_samples - train_size)
+# Y数据进行 z标准化
+Y = (Y - Y.mean()) / Y.std()
 
-# Z-标准化
-mean = np.mean(X_train, axis=0)
-std = np.std(X_train, axis=0)
-X_train = (X_train - mean) / std
-X_test = (X_test - mean) / std
+size = int(0.8 * M)
+X_train = torch.tensor(X_bias[:size], dtype=torch.float32)
+Y_train = torch.tensor(Y[:size], dtype=torch.float32)
+X_test = torch.tensor(X_bias[size:], dtype=torch.float32)
+Y_test = torch.tensor(Y[size:], dtype=torch.float32)
 
-# 转换为 PyTorch 张量
-X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-Y_train_tensor = torch.tensor(Y_train, dtype=torch.float32)
-X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-Y_test_tensor = torch.tensor(Y_test, dtype=torch.float32)
+print('X_train:', X_train.shape)
+print('Y_train:', Y_train.shape)
+print('X_test:', X_test.shape)
+print('Y_test:', Y_test.shape)
 
-print('X_train:', X_train_tensor.shape)
-print('Y_train:', Y_train_tensor.shape)
-print('X_test:', X_test_tensor.shape)
-print('Y_test:', Y_test_tensor.shape)
-
-torch.save((X_train_tensor, Y_train_tensor, X_test_tensor, Y_test_tensor), './data/dataset_3_20D.pt')
+torch.save((X_train, Y_train, X_test, Y_test), './data/dataset_3_20D.pt')
 print('dataset.pt saved')
